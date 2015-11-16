a0ef2b129daceabad4f2ba4b52663e74e5392268
janosmurai <murai.janos@gmail.com>
2015-11-16 21:05:16 +0100

    initial commit

---
+++ codecov_test_main.py
@@ -0,0 +1,15 @@
+__author__ = 'murai'
+
+def test_function(a, b):
+    if a > b:
+        while a > b:
+            b += 1
+        print("a is greater then b")
+    elif a == b:
+        print("a and b are equal")
+    else:
+        print("b is greater then a")
+
+    for i in range(a):
+        if b and (a < 200):
+            print(i * b)
